5ebd3ed8341a19ed8a2ef8266d835780dc40552f
Pawel Szkamruk <pszkamruk@splunk.com>
2023-07-10 10:00:04 +0200

    Revert "check behaviour of cri-o"
    This reverts commit 01f72ee0e47225e2a610501b56967d4bc951fb62.

--- test/k8s_agent_pod_tests/test_agent_correctness_tests.py
+++ test/k8s_agent_pod_tests/test_agent_correctness_tests.py
@@ -140,8 +140,7 @@ def test_all_agent_logs_correctly_ingested_into_splunk(setup):
     match_counter = 0
     for event in events:
         for line in agent_logs:
-            # if event["_raw"].strip() == line.strip():
-            if event["_raw"] == line:
+            if event["_raw"].strip() == line.strip():
                 match_counter += 1
                 break
     assert len(events) == match_counter
