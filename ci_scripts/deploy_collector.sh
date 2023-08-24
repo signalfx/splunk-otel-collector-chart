00e4dcad01bf1f84113bce84a3e5fa6668e57b90
jvoravong <joshvoravong@gmail.com>
2023-08-24 10:52:10 -0600

    Update github workflow e2e/functional tests, Kubernetes version 1.22 -> 1.24-1.28

--- ci_scripts/deploy_collector.sh
+++ ci_scripts/deploy_collector.sh
@@ -10,10 +10,12 @@ else
    helm delete $(helm ls --short)
 fi
 echo "Deploying Splunk OTel Collector for Kubernetes"
+# TODO: Remove networkExplorer.enabled after https://github.com/signalfx/splunk-otel-collector-chart/issues/896
 helm install ci-sck --set splunkPlatform.index=$CI_INDEX_EVENTS \
 --set splunkPlatform.metricsIndex=$CI_INDEX_METRICS \
 --set splunkPlatform.token=$CI_SPLUNK_HEC_TOKEN \
 --set splunkPlatform.endpoint=https://$CI_SPLUNK_HOST:8088/services/collector \
+--set networkExplorer.enabled=${NETWORK_EXPLORER_ENABLED:-false} \
 -f ci_scripts/sck_otel_values.yaml helm-charts/splunk-otel-collector/
 #--set containerLogs.containerRuntime=$CONTAINER_RUNTIME \
 #wait for deployment to finish
