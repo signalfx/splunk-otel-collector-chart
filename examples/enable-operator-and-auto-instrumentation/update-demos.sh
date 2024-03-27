8142f8405be4feaaf141f1f65570dd820410b094
jvoravong <47871238+jvoravong@users.noreply.github.com>
2024-03-27 08:34:47 -0600

    [chore] update spring-petclinic (#1233)
    * improve the update-demos.sh script to remove empty values
    
    * run the update-demos.sh script to get the latest changes from the upstream project for spring-petclinic
    
    * patch
    
    * update demo doc

--- examples/enable-operator-and-auto-instrumentation/update-demos.sh
+++ examples/enable-operator-and-auto-instrumentation/update-demos.sh
@@ -42,6 +42,9 @@ function update_spring_petclinic_demo {
     yq eval -i 'select(.kind == "Deployment") .metadata.labels += {"app.kubernetes.io/part-of": "spring-petclinic"}' "$SPRING_PETCLINIC_PATH"
     yq eval -i 'select(.kind == "Deployment") .spec.template.metadata.labels += {"app.kubernetes.io/part-of": "spring-petclinic"}' "$SPRING_PETCLINIC_PATH"
 
+    # Remove unwanted port settings
+    yq eval -i 'del(.spec.template.spec.containers[].ports[].hostPort) | del(.spec.template.spec.containers[].ports[].protocol)' "$SPRING_PETCLINIC_PATH"
+
     # Remove the downloaded docker-compose file
     rm -rf "$SPRING_PETCLINIC_DOCKER_COMPOSE_PATH"
 
