a29de513dcd208d447a52030ec77bb84e5618ca0
jvoravong <jvoravong@splunk.com>
2024-03-27 08:13:54 -0600

    patch

--- examples/enable-operator-and-auto-instrumentation/update-demos.sh
+++ examples/enable-operator-and-auto-instrumentation/update-demos.sh
@@ -42,8 +42,8 @@ function update_spring_petclinic_demo {
     yq eval -i 'select(.kind == "Deployment") .metadata.labels += {"app.kubernetes.io/part-of": "spring-petclinic"}' "$SPRING_PETCLINIC_PATH"
     yq eval -i 'select(.kind == "Deployment") .spec.template.metadata.labels += {"app.kubernetes.io/part-of": "spring-petclinic"}' "$SPRING_PETCLINIC_PATH"
 
-    # Search and delete keys with empty values or null values
-    yq eval -i 'del(.path.to.key | select(. == {} or . == [] or . == null))' "$SPRING_PETCLINIC_PATH"
+    # Remove unwanted port settings
+    yq eval -i 'del(.spec.template.spec.containers[].ports[].hostPort) | del(.spec.template.spec.containers[].ports[].protocol)' "$SPRING_PETCLINIC_PATH"
 
     # Remove the downloaded docker-compose file
     rm -rf "$SPRING_PETCLINIC_DOCKER_COMPOSE_PATH"
