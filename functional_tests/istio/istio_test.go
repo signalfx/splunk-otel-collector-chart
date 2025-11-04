a16494e986acd58210030bd8e5379c757b95e291
Curtis Robert <crobert@splunk.com>
2025-11-04 13:35:09 -0800

    properly pass test when check succeeds

--- functional_tests/istio/istio_test.go
+++ functional_tests/istio/istio_test.go
@@ -367,6 +367,9 @@ func testIstioMetrics(t *testing.T, expectedMetricsFile string, includeMetricNam
 				pmetrictest.IgnoreMetricAttributeValue("event"),
 				pmetrictest.IgnoreSubsequentDataPoints(metricNames...),
 			)
+			if err == nil {
+				return true
+			}
 			t.Logf("Comparison error: %v", err)
 		}
 
