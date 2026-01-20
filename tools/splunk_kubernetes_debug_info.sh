5867a240421cb65176eab662f348218c51e15f37
Curtis Robert <crobert@splunk.com>
2026-01-20 13:16:47 -0800

    Update expected data, remove debugging

--- tools/splunk_kubernetes_debug_info.sh
+++ tools/splunk_kubernetes_debug_info.sh
@@ -314,10 +314,6 @@ script_end_time=$(date +"%Y-%m-%d %H:%M:%S")
 echo "Script end time: $script_end_time"
 echo "Script end time: $script_end_time" >> "$output_file"
 
-echo "Copying golden files to temp directory"
-
-cp ../functional_tests/functional/testdata/expected_kind_values/expected_kubeletstats_metrics.yaml $temp_dir/
-
 # Create a ZIP archive of all the collected YAML files
 output_zip="splunk_kubernetes_debug_info_$(date +%Y%m%d_%H%M%S).zip"
 echo "Creating ZIP archive: $output_zip"
